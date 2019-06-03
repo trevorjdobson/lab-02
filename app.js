83f0cd691f34ec70640ebb3c545a0ab53cc247bc
trevorjdobson <trevorjdobson@gmail.com>
2019-06-03 16:43:10 -0700

    files created, started on html css and js

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let user = prompt('Hi! Welcome to my page, my name is Trevor Dobson.  What is your name?');
+console.log('The user\'s, name is', user);
+
+let readyBegin = prompt(`Awesome, nice to meet you ${user}, are you ready to begin?`).toLocaleLowerCase();
+console.log('is the user ready?', readyBegin);
+
+
+
+
+
