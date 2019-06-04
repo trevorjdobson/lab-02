1ec21ed6e578e404a8d44383d9e78367d825f283
trevorjdobson <trevorjdobson@gmail.com>
2019-06-04 13:20:15 -0700

    added console logs

--- js/app.js
+++ js/app.js
@@ -8,10 +8,12 @@ let currentAnswer =  'no';
 function readyToBegin(user){
   currentAnswer = prompt(`Are you ready to begin ${user}`).toLowerCase();
 
-  if(currentAnswer === 'yes' || currentAnswer ===  'y'){
+  if(currentAnswer === 'yes' || currentAnswer === 'y'){
     questionTwo(user);
+    console.log(`The user answered ${currentAnswer} for the first question`);
   }else if(currentAnswer === 'no' || currentAnswer === 'n'){
     alert('Too bad we\'re going to begin');
+    console.log(`The user answered ${currentAnswer} for the first question`);
     questionTwo(user);
   }else{
     alert('Please answer with a yes or no');
@@ -24,9 +26,11 @@ function questionTwo(user){
 
   if(currentAnswer === 'yes' || currentAnswer === 'y'){
     alert('Right! I do have a dog');
+    console.log(`The user answered ${currentAnswer} for the 2nd question`);
     questionThree(user);
   }else if(currentAnswer === 'no' || currentAnswer === 'n'){
     alert('Wrong, I do have a dog');
+    console.log(`The user answered ${currentAnswer} for the 2nd question`);
     questionThree(user);
   }else{
     alert('Please answer with a yes or no');
@@ -39,9 +43,11 @@ function questionThree(user){
 
   if(currentAnswer === 'yes' || currentAnswer === 'y'){
     alert('Wrong, I am from Iowa originally');
+    console.log(`The user answered ${currentAnswer} for the 3rd question`);
     questionFour(user);
   }else if(currentAnswer === 'no' || currentAnswer === 'n'){
     alert('Right!, I am from Iowa originally');
+    console.log(`The user answered ${currentAnswer} for the 3rd question`);
     questionFour(user);
   }else{
     alert('Please answer with a yes or no');
@@ -54,9 +60,11 @@ function questionFour(user){
 
   if(currentAnswer === 'yes' || currentAnswer === 'y'){
     alert('Right!, I have a 5 year old daughter, her name is emily');
+    console.log(`The user answered ${currentAnswer} for the 4th question`);
     questionFive(user);
   }else if(currentAnswer === 'no' || currentAnswer === 'n'){
     alert('Wrong, I have a 5 year old daughter named Emily');
+    console.log(`The user answered ${currentAnswer} for the 4th question`);
     questionFive(user);
   }else{
     alert('Please answer with a yes or no');
@@ -67,11 +75,20 @@ function questionFour(user){
 function questionFive(user){
   currentAnswer = prompt(`${user}, this is your last question.  Do I take the ferry to work?`).toLowerCase();
 
-  return currentAnswer === 'yes' || currentAnswer === 'y' ? alert('Good Job, I do take the ferry to work')
-    : currentAnswer === 'no' || currentAnswer === 'n' ? alert('Wrong, I do take the ferry to work')
+  return currentAnswer === 'yes' || currentAnswer === 'y' ? (
+      alert('Good Job, I do take the ferry to work'),
+      alert('Thank you for playing my guessing game!'),
+      console.log(`The user answered ${currentAnswer} for the 5th question`)
+      )
+    : currentAnswer === 'no' || currentAnswer === 'n' ? (
+      alert('Wrong, I do take the ferry to work'),
+      alert('Thank you for playing my game!'),
+      console.log(`The user answered ${currentAnswer} for the 5th question`)
+      )
     : questionFive(user);
 
 }
 
 readyToBegin(user);
+
 
