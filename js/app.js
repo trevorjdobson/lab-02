923ca63e843b8a21e03118aedc30af2f2f661b3e
trevorjdobson <trevorjdobson@gmail.com>
2019-06-05 11:00:43 -0700

    question 6 added and score counter added

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let user = prompt('Hi! Welcome to my page, my name is Trevor Dobson.  What is yo
 console.log('The user\'s, name is', user);
 
 let currentAnswer =  'no';
+let totalCorrect = 0;
 
 function readyToBegin(user){
   currentAnswer = prompt(`Are you ready to begin ${user}`).toLowerCase();
@@ -25,12 +26,13 @@ function questionTwo(user){
   currentAnswer = prompt(`${user}, Do you think I have a dog?`).toLowerCase();
 
   if(currentAnswer === 'yes' || currentAnswer === 'y'){
-    alert('Right! I do have a dog');
-    console.log(`The user answered ${currentAnswer} for the 2nd question`);
+    totalCorrect++;
+    alert(`Right! I do have a dog.  You have answered ${totalCorrect} correct.`);
+    console.log(`The user has ${totalCorrect} right`);
     questionThree(user);
   }else if(currentAnswer === 'no' || currentAnswer === 'n'){
-    alert('Wrong, I do have a dog');
-    console.log(`The user answered ${currentAnswer} for the 2nd question`);
+    alert(`Wrong, I do have a dog. You have answered ${totalCorrect} correct.`);
+    console.log(`The user has ${totalCorrect} right`);
     questionThree(user);
   }else{
     alert('Please answer with a yes or no');
@@ -42,12 +44,13 @@ function questionThree(user){
   currentAnswer = prompt(`${user}, Do you think I am originally from Washington?`).toLowerCase();
 
   if(currentAnswer === 'yes' || currentAnswer === 'y'){
-    alert('Wrong, I am from Iowa originally');
-    console.log(`The user answered ${currentAnswer} for the 3rd question`);
+    alert(`Wrong, I am from Iowa originally.  You have answered ${totalCorrect} correct.`);
+    console.log(`The user has ${totalCorrect} right`);
     questionFour(user);
   }else if(currentAnswer === 'no' || currentAnswer === 'n'){
-    alert('Right!, I am from Iowa originally');
-    console.log(`The user answered ${currentAnswer} for the 3rd question`);
+    totalCorrect++;
+    alert(`Right!, I am from Iowa originally.  You have answered ${totalCorrect} correct.`);
+    console.log(`The user has ${totalCorrect} right`);
     questionFour(user);
   }else{
     alert('Please answer with a yes or no');
@@ -59,12 +62,13 @@ function questionFour(user){
   currentAnswer = prompt(`${user}, Do you think I have a child?`).toLowerCase();
 
   if(currentAnswer === 'yes' || currentAnswer === 'y'){
-    alert('Right!, I have a 5 year old daughter, her name is emily');
-    console.log(`The user answered ${currentAnswer} for the 4th question`);
+    totalCorrect++;
+    alert(`Right!, I have a 5 year old daughter, her name is emily.  You have answered ${totalCorrect} correct.`);
+    console.log(`The user has ${totalCorrect} right`);
     questionFive(user);
   }else if(currentAnswer === 'no' || currentAnswer === 'n'){
-    alert('Wrong, I have a 5 year old daughter named Emily');
-    console.log(`The user answered ${currentAnswer} for the 4th question`);
+    alert(`Wrong, I have a 5 year old daughter named Emily.  You have answered ${totalCorrect} correct.`);
+    console.log(`The user has ${totalCorrect} right`);
     questionFive(user);
   }else{
     alert('Please answer with a yes or no');
@@ -73,20 +77,44 @@ function questionFour(user){
 }
 
 function questionFive(user){
-  currentAnswer = prompt(`${user}, this is your last question.  Do I take the ferry to work?`).toLowerCase();
+  currentAnswer = prompt(`${user}, Do I take the ferry to work?`).toLowerCase();
 
   return currentAnswer === 'yes' || currentAnswer === 'y' ? (
-      alert('Good Job, I do take the ferry to work'),
-      alert('Thank you for playing my guessing game!'),
-      console.log(`The user answered ${currentAnswer} for the 5th question`)
+      totalCorrect++,
+      alert(`Good Job, I do take the ferry to work.  You have answered ${totalCorrect} correct.`),
+      console.log(`The user has ${totalCorrect} right`),
+      questionSix(user)
       )
     : currentAnswer === 'no' || currentAnswer === 'n' ? (
-      alert('Wrong, I do take the ferry to work'),
-      alert('Thank you for playing my game!'),
-      console.log(`The user answered ${currentAnswer} for the 5th question`)
+      alert(`Wrong, I do take the ferry to work.  You have answered ${totalCorrect} correct.`),
+      console.log(`The user has ${totalCorrect} right`),
+      questionSix(user)
       )
     : questionFive(user);
+}
 
+let guesses = 4;
+function questionSix(user){
+  currentAnswer = prompt(`${user}, How many years did I serve in the Army?`).toLowerCase();
+  
+  let correctAnswer =  4;
+  if(guesses <= 0){
+    alert(`No more guesses, You have answered ${totalCorrect} correct.`);
+    questionSeven(user);
+  }else{
+    if(currentAnswer < correctAnswer){
+      guesses--;
+      alert(`Too low, try again.  You have ${guesses} left.`);
+      questionSix(user);
+    }else if(currentAnswer > correctAnswer){
+      guesses--;
+      alert(`Too high, try again.  You have ${guesses} left.`);
+      questionSix(user);
+    }else{
+      totalCorrect++;
+      alert(`You're right! You have now answerwed ${totalCorrect} correct.`);
+    }
+  }
 }
 
 readyToBegin(user);
